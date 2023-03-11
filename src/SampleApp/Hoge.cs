35efe5a79b7165732111cf833117d3f9b4f70501
MareMare <MareMare@users.noreply.github.com>
2023-03-11 15:47:11 +0900

    first commit

---
+++ src/SampleApp/Hoge.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Hoge.cs" company="Harbor Software Corporation">
+// Copyright © 2022 Harbor Software Corporation All rights reserved.
+// Licensed under the Apache-2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using IDisposableGenerator;
+
+namespace SampleApp;
+
+[GenerateIDisposable]
+internal partial class Hoge
+{
+    private readonly MemoryStream _stream = new ();
+    
+    partial void DisposeManagedInstances()
+    {
+        this._stream.Dispose();
+    }
+}
